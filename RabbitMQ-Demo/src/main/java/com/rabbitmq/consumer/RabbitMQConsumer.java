d43014616756865defc81a19cb797e85b56a30ad
Asish Patra <patraasish66@gmail.com>
2025-01-14 16:13:16 +0530

    message consumer created

---
+++ RabbitMQ-Demo/src/main/java/com/rabbitmq/consumer/RabbitMQConsumer.java
@@ -0,0 +1,17 @@
+package com.rabbitmq.consumer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RabbitMQConsumer {
+
+    private static final Logger logger= LoggerFactory.getLogger(RabbitMQConsumer.class);
+
+    @RabbitListener(queues = {"${rabbitmq.queue.name}"})
+    public void consume(String message){
+        logger.info(String.format("Message Received -> %s",message));
+    }
+}
